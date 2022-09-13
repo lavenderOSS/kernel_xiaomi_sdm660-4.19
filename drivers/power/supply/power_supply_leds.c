ubports-clover/android_kernel_xiaomi_clover

drivers/power: Import Xiaomi changes

drivers/power/power_supply_leds.c
@@ -19,6 +19,9 @@
 
 /* Battery specific LEDs triggers. */
 
+#ifdef CONFIG_XIAOMI_CLOVER
+int tp_flag;
+#endif
 static void power_supply_update_bat_leds(struct power_supply *psy)
 {
 	union power_supply_propval status;
@@ -32,6 +35,9 @@ static void power_supply_update_bat_leds(struct power_supply *psy)
 
 	switch (status.intval) {
 	case POWER_SUPPLY_STATUS_FULL:
+#ifdef CONFIG_XIAOMI_CLOVER
+		tp_flag = 1;
+#endif
 		led_trigger_event(psy->charging_full_trig, LED_FULL);
 		led_trigger_event(psy->charging_trig, LED_OFF);
 		led_trigger_event(psy->full_trig, LED_FULL);
@@ -39,6 +45,9 @@ static void power_supply_update_bat_leds(struct power_supply *psy)
 			LED_FULL);
 		break;
 	case POWER_SUPPLY_STATUS_CHARGING:
+#ifdef CONFIG_XIAOMI_CLOVER
+		tp_flag = 2;
+#endif
 		led_trigger_event(psy->charging_full_trig, LED_FULL);
 		led_trigger_event(psy->charging_trig, LED_FULL);
 		led_trigger_event(psy->full_trig, LED_OFF);
@@ -46,6 +55,9 @@ static void power_supply_update_bat_leds(struct power_supply *psy)
 			&delay_on, &delay_off);
 		break;
 	default:
+#ifdef CONFIG_XIAOMI_CLOVER
+		tp_flag = 0;
+#endif
 		led_trigger_event(psy->charging_full_trig, LED_OFF);
 		led_trigger_event(psy->charging_trig, LED_OFF);
 		led_trigger_event(psy->full_trig, LED_OFF);

drivers/power/supply/qcom/fg-core.h
@@ -300,6 +300,9 @@ struct fg_batt_props {
 	char		*batt_profile;
 	int		float_volt_uv;
 	int		vbatt_full_mv;
+#ifdef CONFIG_XIAOMI_CLOVER
+	int		batt_capacity_mah;
+#endif
 	int		fastchg_curr_ma;
 };
 

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -163,6 +163,11 @@ static void fg_encode_default(struct fg_sram_param *sp,
 
 static struct fg_irq_info fg_irqs[FG_IRQ_MAX];
 
+#ifdef CONFIG_XIAOMI_CLOVER
+static int avoid_cool_capacity_jump;
+static int avoid_cool_capacity_time;
+#endif
+
 #define PARAM(_id, _addr_word, _addr_byte, _len, _num, _den, _offset,	\
 	      _enc, _dec)						\
 	[FG_SRAM_##_id] = {						\
@@ -902,11 +907,31 @@ static int fg_get_prop_capacity(struct fg_chip *chip, int *val)
 		return 0;
 	}
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	if ((chip->charge_status == POWER_SUPPLY_STATUS_FULL) && (chip->health == POWER_SUPPLY_HEALTH_COOL)) {
+		*val = FULL_CAPACITY;
+		return 0;
+	}
+#endif
+
 	rc = fg_get_msoc(chip, &msoc);
 	if (rc < 0)
 		return rc;
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	if ((chip->charge_status == POWER_SUPPLY_STATUS_FULL) && (msoc == 99)){
+		*val = FULL_CAPACITY;
+		return 0;
+	}
+#endif
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	if ((avoid_cool_capacity_jump == 1) && (avoid_cool_capacity_time <= 21) && (chip->health == POWER_SUPPLY_HEALTH_COOL) && (!is_input_present(chip))) {
+		*val = FULL_CAPACITY;
+	} else if (chip->dt.linearize_soc && chip->delta_soc > 0)
+#else
 	if (chip->dt.linearize_soc && chip->delta_soc > 0)
+#endif
 		*val = chip->maint_soc;
 	else
 		*val = msoc;
@@ -1028,6 +1053,15 @@ static int fg_get_batt_profile(struct fg_chip *chip)
 		pr_err("battery cc_cv threshold unavailable, rc:%d\n", rc);
 		chip->bp.vbatt_full_mv = -EINVAL;
 	}
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	rc = of_property_read_u32(profile_node, "qcom,nom-batt-capacity-mah",
+			&chip->bp.batt_capacity_mah);
+	if (rc < 0) {
+		pr_err("battery capacity mah unavailable, rc:%d\n", rc);
+		chip->bp.batt_capacity_mah = -EINVAL;
+	}
+#endif
 
 	data = of_get_property(profile_node, "qcom,fg-profile-data", &len);
 	if (!data) {
@@ -1831,7 +1865,11 @@ static int fg_charge_full_update(struct fg_chip *chip)
 		msoc, bsoc, chip->health, chip->charge_status,
 		chip->charge_full);
 	if (chip->charge_done && !chip->charge_full) {
+#ifdef CONFIG_XIAOMI_CLOVER
+		if (msoc >= 99 && (chip->health == POWER_SUPPLY_HEALTH_GOOD || chip->health == POWER_SUPPLY_HEALTH_COOL)) {
+#else
 		if (msoc >= 99 && chip->health == POWER_SUPPLY_HEALTH_GOOD) {
+#endif
 			fg_dbg(chip, FG_STATUS, "Setting charge_full to true\n");
 			chip->charge_full = true;
 			/*
@@ -2745,6 +2783,12 @@ static void status_change_work(struct work_struct *work)
 	chip->charge_done = prop.intval;
 	fg_cycle_counter_update(chip);
 	fg_cap_learning_update(chip);
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	if ((chip->charge_status == POWER_SUPPLY_STATUS_FULL) && (chip->health == POWER_SUPPLY_HEALTH_COOL)) {
+		avoid_cool_capacity_jump = 1;
+	}
+#endif
 
 	rc = fg_charge_full_update(chip);
 	if (rc < 0)
@@ -3719,6 +3763,17 @@ static void ttf_work(struct work_struct *work)
 		}
 	}
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	if ((chip->health == POWER_SUPPLY_HEALTH_COOL) && (avoid_cool_capacity_jump == 1) && (!is_input_present(chip))) {
+		avoid_cool_capacity_time++;
+	}
+
+	if ((avoid_cool_capacity_time >= 22) || (chip->health != POWER_SUPPLY_HEALTH_COOL)) {
+		avoid_cool_capacity_time = 0;
+		avoid_cool_capacity_jump = 0;
+	}
+#endif
+
 	/* recurse every 10 seconds */
 	schedule_delayed_work(&chip->ttf_work, msecs_to_jiffies(10000));
 end_work:
@@ -3789,7 +3844,11 @@ static int fg_psy_get_property(struct power_supply *psy,
 		rc = fg_get_sram_prop(chip, FG_SRAM_OCV, &pval->intval);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+#ifdef CONFIG_XIAOMI_CLOVER
+		pval->intval = chip->bp.batt_capacity_mah;
+#else
 		pval->intval = chip->cl.nom_cap_uah;
+#endif
 		break;
 	case POWER_SUPPLY_PROP_RESISTANCE_ID:
 		pval->intval = chip->batt_id_ohms;
@@ -4827,19 +4886,35 @@ static int fg_parse_ki_coefficients(struct fg_chip *chip)
 	return 0;
 }
 
+#ifdef CONFIG_XIAOMI_CLOVER
+#define DEFAULT_CUTOFF_VOLT_MV		3400
+#define DEFAULT_EMPTY_VOLT_MV		3400
+#define DEFAULT_RECHARGE_VOLT_MV	4360
+#define DEFAULT_CHG_TERM_CURR_MA	200
+#else
 #define DEFAULT_CUTOFF_VOLT_MV		3200
 #define DEFAULT_EMPTY_VOLT_MV		2850
 #define DEFAULT_RECHARGE_VOLT_MV	4250
 #define DEFAULT_CHG_TERM_CURR_MA	100
+#endif
 #define DEFAULT_CHG_TERM_BASE_CURR_MA	75
+#ifdef CONFIG_XIAOMI_CLOVER
+#define DEFAULT_SYS_TERM_CURR_MA	-225
+#else
 #define DEFAULT_SYS_TERM_CURR_MA	-125
+#endif
 #define DEFAULT_CUTOFF_CURR_MA		500
 #define DEFAULT_DELTA_SOC_THR		1
 #define DEFAULT_RECHARGE_SOC_THR	95
 #define DEFAULT_BATT_TEMP_COLD		0
 #define DEFAULT_BATT_TEMP_COOL		5
+#ifdef CONFIG_XIAOMI_CLOVER
+#define DEFAULT_BATT_TEMP_WARM		70
+#define DEFAULT_BATT_TEMP_HOT		75
+#else
 #define DEFAULT_BATT_TEMP_WARM		45
 #define DEFAULT_BATT_TEMP_HOT		50
+#endif
 #define DEFAULT_CL_START_SOC		15
 #define DEFAULT_CL_MIN_TEMP_DECIDEGC	150
 #define DEFAULT_CL_MAX_TEMP_DECIDEGC	450

drivers/power/supply/qcom/qpnp-smb2.c
@@ -29,6 +29,18 @@
 #include "storm-watch.h"
 #include <linux/pmic-voter.h>
 
+#ifdef CONFIG_XIAOMI_CLOVER
+#include <linux/proc_fs.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+#include <linux/slab.h>
+#define LENGTH 32
+#define CHG_NAME "driver/chg_enable"
+static int charger_limit_enbale = 1;
+static struct proc_dir_entry *proc_chg;
+#endif
+
 #define SMB2_DEFAULT_WPWR_UW	8000000
 
 static struct smb_params v1_params = {
@@ -179,6 +191,9 @@ struct smb2 {
 	bool			bad_part;
 };
 
+#ifdef CONFIG_XIAOMI_CLOVER
+struct smb_charger *smbchg_dev;
+#endif
 static int __debug_mask;
 module_param_named(
 	debug_mask, __debug_mask, int, S_IRUSR | S_IWUSR
@@ -344,6 +359,9 @@ static enum power_supply_property smb2_usb_props[] = {
 	POWER_SUPPLY_PROP_CURRENT_MAX,
 	POWER_SUPPLY_PROP_TYPE,
 	POWER_SUPPLY_PROP_TYPEC_MODE,
+#ifdef CONFIG_XIAOMI_CLOVER
+	POWER_SUPPLY_PROP_USB_OTG,
+#endif
 	POWER_SUPPLY_PROP_TYPEC_POWER_ROLE,
 	POWER_SUPPLY_PROP_TYPEC_CC_ORIENTATION,
 	POWER_SUPPLY_PROP_PD_ALLOWED,
@@ -396,6 +414,11 @@ static int smb2_usb_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 		rc = smblib_get_prop_usb_voltage_now(chg, val);
 		break;
+#ifdef CONFIG_XIAOMI_CLOVER
+	case POWER_SUPPLY_PROP_USB_OTG:
+		rc = smblib_get_chg_otg_present(chg, val);
+		break;
+#endif
 	case POWER_SUPPLY_PROP_PD_CURRENT_MAX:
 		val->intval = get_client_vote(chg->usb_icl_votable, PD_VOTER);
 		break;
@@ -931,6 +954,11 @@ static enum power_supply_property smb2_batt_props[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_MAX,
 	POWER_SUPPLY_PROP_VOLTAGE_QNOVO,
 	POWER_SUPPLY_PROP_CURRENT_NOW,
+#ifdef CONFIG_XIAOMI_CLOVER
+	POWER_SUPPLY_PROP_RESISTANCE_ID,
+	POWER_SUPPLY_PROP_CHARGING_ENABLED,
+	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
+#endif
 	POWER_SUPPLY_PROP_CURRENT_QNOVO,
 	POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX,
 	POWER_SUPPLY_PROP_TEMP,
@@ -1012,6 +1040,21 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		val->intval = get_client_vote_locked(chg->fv_votable,
 				QNOVO_VOTER);
 		break;
+#ifdef CONFIG_XIAOMI_CLOVER
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		val->intval = get_effective_result(chg->usb_icl_votable);
+		if (val->intval < 0) /* no votes */
+			val->intval = 1;
+		else
+			val->intval = !!val->intval;
+		break;
+	case POWER_SUPPLY_PROP_RESISTANCE_ID:
+		rc = smblib_get_prop_batt_resistance_id(chg, val);
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+		rc = smblib_get_prop_batt_charge_full_design(chg, val);
+		break;
+#endif
 	case POWER_SUPPLY_PROP_CURRENT_QNOVO:
 		val->intval = get_client_vote_locked(chg->fcc_votable,
 				QNOVO_VOTER);
@@ -1021,7 +1064,11 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 					      BATT_PROFILE_VOTER);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
+#ifdef CONFIG_XIAOMI_CLOVER
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
+#else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+#endif
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_DONE:
 		rc = smblib_get_prop_batt_charge_done(chg, val);
@@ -1142,6 +1189,11 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_RERUN_AICL:
 		rc = smblib_rerun_aicl(chg);
 		break;
+#ifdef CONFIG_XIAOMI_CLOVER
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		rc = vote(chg->usb_icl_votable,DEFAULT_VOTER, !val->intval, 0);
+		break;
+#endif
 	case POWER_SUPPLY_PROP_DP_DM:
 		rc = smblib_dp_dm(chg, val->intval);
 		break;
@@ -1558,7 +1610,11 @@ static int smb2_init_hw(struct smb2 *chip)
 	vote(chg->pd_disallowed_votable_indirect, HVDCP_TIMEOUT_VOTER,
 			true, 0);
 	vote(chg->pd_disallowed_votable_indirect, MICRO_USB_VOTER,
+#ifdef CONFIG_XIAOMI_CLOVER
+			true, 0);
+#else
 			chg->micro_usb_mode, 0);
+#endif
 	vote(chg->hvdcp_enable_votable, MICRO_USB_VOTER,
 			chg->micro_usb_mode, 0);
 
@@ -2244,6 +2300,63 @@ static void smb2_create_debugfs(struct smb2 *chip)
 
 #endif
 
+#ifdef CONFIG_XIAOMI_CLOVER
+struct smb_charger *chip_b;
+
+static ssize_t chg_read_proc_enable(struct file *file, char __user *page, size_t size, loff_t *ppos)
+{
+	char *ptr = page;
+	if (*ppos)
+		return 0;
+
+	ptr += sprintf(ptr, "%d\n", charger_limit_enbale);
+
+	*ppos += ptr - page;
+	return (ptr - page);
+}
+static ssize_t chg_write_proc_enable(struct file *file, const char __user *buff, size_t size, loff_t *ppos)
+{
+	char write_buff[LENGTH] = {0};
+	int rc = 0;
+
+	if (size > LENGTH)
+		return -EFAULT;
+	if (copy_from_user(&write_buff,buff,size))
+		return -ENOMEM;
+
+	if (write_buff[0] == '1') {
+		charger_limit_enbale = 1;
+		rc = vote(chip_b->chg_disable_votable, DEFAULT_VOTER, !charger_limit_enbale, 0);
+		pr_info("chg set enable\n");
+		if (rc < 0)
+			pr_debug("chg set enable charging fail\n");
+	} else if (write_buff[0] == '0') {
+		charger_limit_enbale = 0;
+		rc = vote(chip_b->chg_disable_votable, DEFAULT_VOTER, !charger_limit_enbale, 0);
+		pr_info("chg set disenable\n");
+		if (rc < 0)
+			pr_debug("chg set disenable charging fail\n");
+	}
+	return size;
+}
+
+static struct file_operations file_ops = {
+	.read = chg_read_proc_enable,
+	.write = chg_write_proc_enable,
+};
+
+static int proc_create_chg_enable(void)
+{
+	int rc = 0;
+
+	proc_chg = proc_create(CHG_NAME,0664,NULL, &file_ops);
+	if (proc_chg == NULL)
+		return -ENOMEM;
+
+	return rc;
+}
+#endif
+
 static int smb2_probe(struct platform_device *pdev)
 {
 	struct smb2 *chip;
@@ -2266,6 +2379,9 @@ static int smb2_probe(struct platform_device *pdev)
 	chg->irq_info = smb2_irqs;
 	chg->name = "PMI";
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	smbchg_dev = chg;
+#endif
 	chg->regmap = dev_get_regmap(chg->dev->parent, NULL);
 	if (!chg->regmap) {
 		pr_err("parent regmap is missing\n");
@@ -2411,6 +2527,10 @@ static int smb2_probe(struct platform_device *pdev)
 
 	device_init_wakeup(chg->dev, true);
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	chip_b = chg;
+	proc_create_chg_enable();
+#endif
 	pr_info("QPNP SMB2 probed successfully usb:present=%d type=%d batt:present = %d health = %d charge = %d\n",
 		usb_present, chg->real_charger_type,
 		batt_present, batt_health, batt_charge_type);
@@ -2450,6 +2570,9 @@ static int smb2_remove(struct platform_device *pdev)
 	regulator_unregister(chg->vconn_vreg->rdev);
 	regulator_unregister(chg->vbus_vreg->rdev);
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	proc_remove(proc_chg);
+#endif
 	platform_set_drvdata(pdev, NULL);
 	return 0;
 }

lavenderOSS/kernel_xiaomi_sdm660-4.19

(cherry picked from commit 199d53f292aa01ce4b73837bfcb4983bd897f1eb)
Signed-off-by: pix106 <sbordenave@gmail.com>

drivers/power/supply/power_supply_leds.c
@@ -19,6 +19,9 @@
 
 /* Battery specific LEDs triggers. */
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+int tp_flag;
+#endif
 static void power_supply_update_bat_leds(struct power_supply *psy)
 {
 	union power_supply_propval status;
@@ -32,6 +35,9 @@ static void power_supply_update_bat_leds(struct power_supply *psy)
 
 	switch (status.intval) {
 	case POWER_SUPPLY_STATUS_FULL:
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		tp_flag = 1;
+#endif
 		led_trigger_event(psy->charging_full_trig, LED_FULL);
 		led_trigger_event(psy->charging_trig, LED_OFF);
 		led_trigger_event(psy->full_trig, LED_FULL);
@@ -39,6 +45,9 @@ static void power_supply_update_bat_leds(struct power_supply *psy)
 			LED_FULL);
 		break;
 	case POWER_SUPPLY_STATUS_CHARGING:
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		tp_flag = 2;
+#endif
 		led_trigger_event(psy->charging_full_trig, LED_FULL);
 		led_trigger_event(psy->charging_trig, LED_FULL);
 		led_trigger_event(psy->full_trig, LED_OFF);
@@ -46,6 +55,9 @@ static void power_supply_update_bat_leds(struct power_supply *psy)
 			&delay_on, &delay_off);
 		break;
 	default:
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		tp_flag = 0;
+#endif
 		led_trigger_event(psy->charging_full_trig, LED_OFF);
 		led_trigger_event(psy->charging_trig, LED_OFF);
 		led_trigger_event(psy->full_trig, LED_OFF);

drivers/power/supply/qcom/fg-core.h
@@ -331,6 +331,9 @@ struct fg_batt_props {
 	char		*batt_profile;
 	int		float_volt_uv;
 	int		vbatt_full_mv;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	int		batt_capacity_mah;
+#endif
 	int		fastchg_curr_ma;
 	int		*therm_coeffs;
 	int		therm_ctr_offset;

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -242,6 +242,11 @@ struct fg_gen3_chip {
 	bool			slope_limit_en;
 };
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+static int avoid_cool_capacity_jump;
+static int avoid_cool_capacity_time;
+#endif
+
 static struct fg_sram_param pmi8998_v1_sram_params[] = {
 	PARAM(BATT_SOC, BATT_SOC_WORD, BATT_SOC_OFFSET, 4, 1, 1, 0, NULL,
 		fg_decode_default),
@@ -817,7 +822,18 @@ static int fg_get_prop_capacity(struct fg_dev *fg, int *val)
 	if (rc < 0)
 		return rc;
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	if ((fg->charge_full) && ((fg->health == POWER_SUPPLY_HEALTH_COOL) || (msoc == 99))) {
+		*val = FULL_CAPACITY;
+		return 0;
+	}
+
+	if ((avoid_cool_capacity_jump == 1) && (avoid_cool_capacity_time <= 21) && (fg->health == POWER_SUPPLY_HEALTH_COOL) && (!is_input_present(fg))) {
+		*val = FULL_CAPACITY;
+	} else if (chip->dt.linearize_soc && fg->delta_soc > 0)
+#else
 	if (chip->dt.linearize_soc && fg->delta_soc > 0)
+#endif
 		*val = fg->maint_soc;
 	else
 		*val = msoc;
@@ -976,6 +992,22 @@ static int fg_get_batt_profile(struct fg_dev *fg)
 		pr_err("battery cc_cv threshold unavailable, rc:%d\n", rc);
 		fg->bp.vbatt_full_mv = -EINVAL;
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	rc = of_property_read_u32(profile_node, "qcom,nom-batt-capacity-mah",
+			&fg->bp.batt_capacity_mah);
+	if (rc < 0) {
+		pr_err("battery capacity mah unavailable, rc:%d\n", rc);
+		fg->bp.batt_capacity_mah = -EINVAL;
+	}
+#else
+        rc = of_property_read_u32(profile_node, "qcom,fg-cc-cv-threshold-mv",
+                        &fg->bp.vbatt_full_mv);
+        if (rc < 0) {
+                pr_err("battery cc_cv threshold unavailable, rc:%d\n", rc);
+                fg->bp.vbatt_full_mv = -EINVAL;
+        }
+#endif
 
 	data = of_get_property(profile_node, "qcom,fg-profile-data", &len);
 	if (!data) {
@@ -1679,9 +1711,15 @@ static int fg_charge_full_update(struct fg_dev *fg)
 		msoc, bsoc, fg->health, fg->charge_status,
 		fg->charge_full);
 	if (fg->charge_done && !fg->charge_full) {
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		if (msoc >= 99 && (fg->health == POWER_SUPPLY_HEALTH_GOOD || fg->health == POWER_SUPPLY_HEALTH_COOL)) {
+#else
 		if (msoc >= 99 && fg->health == POWER_SUPPLY_HEALTH_GOOD) {
-			fg_dbg(fg, FG_STATUS, "Setting charge_full to true\n");
-			fg->charge_full = true;
+#endif
+                        fg_dbg(fg, FG_STATUS, "Setting charge_full to true\n");
+                        fg->charge_full = true;
+
+
 			/*
 			 * Lower the recharge voltage so that VBAT_LT_RECHG
 			 * signal will not be asserted soon.
@@ -2804,6 +2842,12 @@ static void status_change_work(struct work_struct *work)
 	}
 #endif
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	if ((fg->charge_full) && (fg->health == POWER_SUPPLY_HEALTH_COOL)) {
+		avoid_cool_capacity_jump = 1;
+	}
+#endif
+
 	rc = fg_charge_full_update(fg);
 	if (rc < 0)
 		pr_err("Error in charge_full_update, rc=%d\n", rc);
@@ -3797,6 +3841,17 @@ static void ttf_work(struct work_struct *work)
 		}
 	}
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	if ((fg->health == POWER_SUPPLY_HEALTH_COOL) && (avoid_cool_capacity_jump == 1) && (!is_input_present(fg))) {
+		avoid_cool_capacity_time++;
+	}
+
+	if ((avoid_cool_capacity_time >= 22) || (fg->health != POWER_SUPPLY_HEALTH_COOL)) {
+		avoid_cool_capacity_time = 0;
+		avoid_cool_capacity_jump = 0;
+	}
+#endif
+
 	/* recurse every 10 seconds */
 	queue_delayed_work(system_power_efficient_wq, &chip->ttf_work, msecs_to_jiffies(10000));
 end_work:
@@ -3876,7 +3931,11 @@ static int fg_psy_get_property(struct power_supply *psy,
 		rc = fg_get_sram_prop(fg, FG_SRAM_OCV, &pval->intval);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		pval->intval = fg->bp.batt_capacity_mah;
+#else
 		pval->intval = chip->cl.nom_cap_uah;
+#endif
 		break;
 	case POWER_SUPPLY_PROP_RESISTANCE_ID:
 		pval->intval = fg->batt_id_ohms;
@@ -5115,19 +5174,35 @@ static void fg_create_debugfs(struct fg_dev *fg)
 }
 #endif
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#define DEFAULT_CUTOFF_VOLT_MV		3400
+#define DEFAULT_EMPTY_VOLT_MV		3400
+#define DEFAULT_RECHARGE_VOLT_MV	4360
+#define DEFAULT_CHG_TERM_CURR_MA	200
+#else
 #define DEFAULT_CUTOFF_VOLT_MV		3200
 #define DEFAULT_EMPTY_VOLT_MV		2850
 #define DEFAULT_RECHARGE_VOLT_MV	4250
 #define DEFAULT_CHG_TERM_CURR_MA	100
+#endif
 #define DEFAULT_CHG_TERM_BASE_CURR_MA	75
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#define DEFAULT_SYS_TERM_CURR_MA	-225
+#else
 #define DEFAULT_SYS_TERM_CURR_MA	-125
+#endif
 #define DEFAULT_CUTOFF_CURR_MA		500
 #define DEFAULT_DELTA_SOC_THR		1
 #define DEFAULT_RECHARGE_SOC_THR	95
 #define DEFAULT_BATT_TEMP_COLD		0
 #define DEFAULT_BATT_TEMP_COOL		5
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#define DEFAULT_BATT_TEMP_WARM		70
+#define DEFAULT_BATT_TEMP_HOT		75
+#else
 #define DEFAULT_BATT_TEMP_WARM		45
 #define DEFAULT_BATT_TEMP_HOT		50
+#endif
 #define DEFAULT_CL_START_SOC		15
 #define DEFAULT_CL_MIN_TEMP_DECIDEGC	150
 #define DEFAULT_CL_MAX_TEMP_DECIDEGC	500

drivers/power/supply/qcom/qpnp-smb2.c
@@ -50,6 +50,18 @@ extern int hwc_check_global;
 extern bool is_poweroff_charge;
 #endif
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#include <linux/proc_fs.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+#include <linux/slab.h>
+#define LENGTH 32
+#define CHG_NAME "driver/chg_enable"
+static int charger_limit_enbale = 1;
+static struct proc_dir_entry *proc_chg;
+#endif
+
 #define SMB2_DEFAULT_WPWR_UW	8000000
 
 static struct smb_params v1_params = {
@@ -202,6 +214,11 @@ struct smb2 {
 };
 
 static int __debug_mask;
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+struct smb_charger *smbchg_dev;
+#endif
+
 
 static int __weak_chg_icl_ua = 500000;
 static ssize_t weak_chg_icl_ua_show(struct device *dev, struct device_attribute
@@ -453,6 +470,9 @@ static enum power_supply_property smb2_usb_props[] = {
 	POWER_SUPPLY_PROP_CURRENT_MAX,
 	POWER_SUPPLY_PROP_TYPE,
 	POWER_SUPPLY_PROP_TYPEC_MODE,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	POWER_SUPPLY_PROP_USB_OTG,
+#endif
 	POWER_SUPPLY_PROP_TYPEC_POWER_ROLE,
 	POWER_SUPPLY_PROP_TYPEC_CC_ORIENTATION,
 	POWER_SUPPLY_PROP_TYPEC_SRC_RP,
@@ -517,6 +537,11 @@ static int smb2_usb_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 		rc = smblib_get_prop_usb_voltage_now(chg, val);
 		break;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	case POWER_SUPPLY_PROP_USB_OTG:
+		rc = smblib_get_chg_otg_present(chg, val);
+		break;
+#endif
 	case POWER_SUPPLY_PROP_PD_CURRENT_MAX:
 		val->intval = get_client_vote(chg->usb_icl_votable, PD_VOTER);
 		break;
@@ -1108,6 +1133,10 @@ static enum power_supply_property smb2_batt_props[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_MAX,
 	POWER_SUPPLY_PROP_VOLTAGE_QNOVO,
 	POWER_SUPPLY_PROP_CURRENT_NOW,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	POWER_SUPPLY_PROP_RESISTANCE_ID,
+	POWER_SUPPLY_PROP_CHARGING_ENABLED,
+#endif
 	POWER_SUPPLY_PROP_CURRENT_QNOVO,
 	POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX,
 #ifndef CONFIG_MACH_LONGCHEER
@@ -1207,6 +1236,21 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		val->intval = get_client_vote_locked(chg->fv_votable,
 				QNOVO_VOTER);
 		break;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		val->intval = get_effective_result(chg->usb_icl_votable);
+		if (val->intval < 0) /* no votes */
+			val->intval = 1;
+		else
+			val->intval = !!val->intval;
+		break;
+	case POWER_SUPPLY_PROP_RESISTANCE_ID:
+		rc = smblib_get_prop_batt_resistance_id(chg, val);
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+		rc = smblib_get_prop_batt_charge_full_design(chg, val);
+		break;
+#endif
 	case POWER_SUPPLY_PROP_CURRENT_QNOVO:
 		val->intval = get_client_vote_locked(chg->fcc_votable,
 				QNOVO_VOTER);
@@ -1222,7 +1266,7 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		break;
 #endif
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-#ifdef CONFIG_MACH_LONGCHEER
+#if defined(CONFIG_MACH_LONGCHEER) || defined(CONFIG_MACH_XIAOMI_CLOVER)
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
 #else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
@@ -1258,9 +1302,6 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
-#ifndef CONFIG_MACH_LONGCHEER
-	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
-#endif
 	case POWER_SUPPLY_PROP_CYCLE_COUNT:
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 	case POWER_SUPPLY_PROP_TEMP:
@@ -1373,6 +1414,11 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_RERUN_AICL:
 		rc = smblib_rerun_aicl(chg);
 		break;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		rc = vote(chg->usb_icl_votable,DEFAULT_VOTER, !val->intval, 0);
+		break;
+#endif
 	case POWER_SUPPLY_PROP_DP_DM:
 		rc = smblib_dp_dm(chg, val->intval);
 		break;
@@ -1900,7 +1946,11 @@ static int smb2_init_hw(struct smb2 *chip)
 	vote(chg->hvdcp_disable_votable_indirect, VBUS_CC_SHORT_VOTER,
 		(chg->connector_type == POWER_SUPPLY_CONNECTOR_TYPEC), 0);
 	vote(chg->pd_disallowed_votable_indirect, MICRO_USB_VOTER,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+                        true, 0);
+#else
 		(chg->connector_type == POWER_SUPPLY_CONNECTOR_MICRO_USB), 0);
+#endif
 	vote(chg->hvdcp_enable_votable, MICRO_USB_VOTER,
 		(chg->connector_type == POWER_SUPPLY_CONNECTOR_MICRO_USB), 0);
 
@@ -2723,6 +2773,63 @@ static int lct_unregister_powermanager(struct smb_charger *chg)
 	return 0;
 }
 #endif
+#endif
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+struct smb_charger *chip_b;
+
+static ssize_t chg_read_proc_enable(struct file *file, char __user *page, size_t size, loff_t *ppos)
+{
+	char *ptr = page;
+	if (*ppos)
+		return 0;
+
+	ptr += sprintf(ptr, "%d\n", charger_limit_enbale);
+
+	*ppos += ptr - page;
+	return (ptr - page);
+}
+static ssize_t chg_write_proc_enable(struct file *file, const char __user *buff, size_t size, loff_t *ppos)
+{
+	char write_buff[LENGTH] = {0};
+	int rc = 0;
+
+	if (size > LENGTH)
+		return -EFAULT;
+	if (copy_from_user(&write_buff,buff,size))
+		return -ENOMEM;
+
+	if (write_buff[0] == '1') {
+		charger_limit_enbale = 1;
+		rc = vote(chip_b->chg_disable_votable, DEFAULT_VOTER, !charger_limit_enbale, 0);
+		pr_info("chg set enable\n");
+		if (rc < 0)
+			pr_debug("chg set enable charging fail\n");
+	} else if (write_buff[0] == '0') {
+		charger_limit_enbale = 0;
+		rc = vote(chip_b->chg_disable_votable, DEFAULT_VOTER, !charger_limit_enbale, 0);
+		pr_info("chg set disenable\n");
+		if (rc < 0)
+			pr_debug("chg set disenable charging fail\n");
+	}
+	return size;
+}
+
+static struct file_operations file_ops = {
+	.read = chg_read_proc_enable,
+	.write = chg_write_proc_enable,
+};
+
+static int proc_create_chg_enable(void)
+{
+	int rc = 0;
+
+	proc_chg = proc_create(CHG_NAME,0664,NULL, &file_ops);
+	if (proc_chg == NULL)
+		return -ENOMEM;
+
+	return rc;
+}
+#endif
 
 static int smb2_probe(struct platform_device *pdev)
 {
@@ -2751,6 +2858,9 @@ static int smb2_probe(struct platform_device *pdev)
 	chg->name = "PMI";
 	chg->audio_headset_drp_wait_ms = &__audio_headset_drp_wait_ms;
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	smbchg_dev = chg;
+#endif
 	chg->regmap = dev_get_regmap(chg->dev->parent, NULL);
 	if (!chg->regmap) {
 		pr_err("parent regmap is missing\n");
@@ -2922,6 +3032,10 @@ static int smb2_probe(struct platform_device *pdev)
 	chg->charging_enabled = true;
 #endif
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	chip_b = chg;
+	proc_create_chg_enable();
+#endif
 	pr_info("QPNP SMB2 probed successfully usb:present=%d type=%d batt:present = %d health = %d charge = %d\n",
 		usb_present, chg->real_charger_type,
 		batt_present, batt_health, batt_charge_type);
@@ -2970,6 +3084,10 @@ static int smb2_remove(struct platform_device *pdev)
 	regulator_unregister(chg->vbus_vreg->rdev);
 	sysfs_remove_groups(&chg->dev->kobj, smb2_groups);
 	debugfs_remove_recursive(chip->dfs_root);
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	proc_remove(proc_chg);
+#endif
 	platform_set_drvdata(pdev, NULL);
 	return 0;
 }